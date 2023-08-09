e6bee11c678bbbb560865d26e28635def70ef5fb
Anton Tarasov <antoha@Antons-MacBook-Pro.local>
2023-08-09 21:33:27 +0700

    added expression struct and functions for performing math operations

--- main.go
+++ main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Expression struct {
+	X, Y     int
+	Operator string
+}
+
+func (exp Expression) Add() int {
+	return exp.X + exp.Y
+}
+
+func (exp Expression) Subtract() int {
+	return exp.X - exp.Y
+}
+
+func (exp Expression) Multiply() int {
+	return exp.X * exp.Y
+}
+
+func (exp Expression) Divide() int {
+	return exp.X / exp.Y
+}
 
 func main() {
-	welcomeMessage := "Hi, I'm a calculator. What do you want to do?"
-	fmt.Println(welcomeMessage)
+	// declaring a var that will read and store input from stdin
+	buffer := bufio.NewReader(os.Stdin)
+
+	welcomeMessage := "What do you want to calculate?"
+
+	for {
+		fmt.Println(welcomeMessage)
+		usrInputStr, _ := buffer.ReadString('\n')
+		usrInputStr = strings.TrimSpace(usrInputStr)
+		usrInputNum, _ := strconv.Atoi(usrInputStr)
+		fmt.Println(usrInputNum + 8)
+	}
 }
